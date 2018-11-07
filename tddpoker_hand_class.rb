7dde4f586487497e6dd1bbe964e31d9cdd31baa1
Nicci Sheets <niccisheets@aol.com>
2018-11-07 15:08:19 -0500

    got the testing for sorting the cards

--- tddpoker_hand_class.rb
+++ tddpoker_hand_class.rb
@@ -9,7 +9,7 @@ class TddPokerHandClass < Minitest::Test
 		end
 
 
-		def test_hand_is_getting_dealt_cards
+		def test_hand_is_getting_dealt_cards_randomized_for_game_use
 			deck = Deck.all_cards.shuffle
 			x = Hand.deal(deck)
 			assert_equal(Hand, x.class)
@@ -17,20 +17,29 @@ class TddPokerHandClass < Minitest::Test
 		end
 
 
+		def test_cards_in_are_sorted_regarding_poker_values
+			cards = [Card.new(:eight, :clubs), Card.new(:three, :clubs), Card.new(:four, :clubs), Card.new(:five, :clubs), Card.new(:six, :clubs)]
+			deck = Deck.new(cards)
+			hand = Hand.deal(deck)
+			cards_sorted = [Card.new(:three, :clubs), Card.new(:four, :clubs), Card.new(:five, :clubs), Card.new(:six, :clubs), Card.new(:eight, :clubs)]
+			deck_sorted = Deck.new(cards_sorted)
+			hand_sorted = Hand.deal(deck_sorted)
+			assert_equal(hand.cards, hand_sorted.cards)
+		end
+
 		def test_cards_dup_creates_duplicates
 			cards = [Card.new(:two, :clubs), Card.new(:three, :clubs), Card.new(:four, :clubs), Card.new(:five, :clubs), Card.new(:six, :clubs)]
 			assert_equal(cards, cards.dup)
 		end
 
 
-		# def test_cards_in_hand
-		# 	cards = [Card.new(:eight, :clubs), Card.new(:three, :clubs), Card.new(:four, :clubs), Card.new(:five, :clubs), Card.new(:six, :clubs)]
-  #    		deck = Deck.new(cards.dup)
-  #     		hand = Hand.deal(deck)
-  #     		assert_equal(5, cards.length)
-  #     		assert_equal(1, hand.length)
-  #     		# assert_equal(hand.cards, cards)
-  #       end
+		def test_cards_in_hand_can_be_manipulated_for_testing
+			cards = [Card.new(:eight, :clubs), Card.new(:three, :clubs), Card.new(:four, :clubs), Card.new(:five, :clubs), Card.new(:six, :clubs)]
+     		deck = Deck.new(cards.dup)
+      		hand = Hand.deal(deck)
+      		assert_equal(Hand, hand.class)
+      		assert_equal(hand.cards, hand.cards_to_test)
+        end
 
 
 end
