ac5a1f20a128c2afc5b778fd3228012794abef0d
Nicci Sheets <niccisheets@aol.com>
2018-11-07 14:26:37 -0500

    fixed the issue with the Hand class and .cards and .cards_to_test methods

--- poker_hand_class.rb
+++ poker_hand_class.rb
@@ -1,13 +1,14 @@
 require_relative "poker_card_class.rb"
 require_relative "poker_deck_class.rb"
-require_relative "poker_test_hand_class.rb"
 
-class Hand
+# may need to comment out the @cards_to_test = cards.dup (line 12) and remove :cards_to_test (line 17), if putting through Sinatra 
+
+class Hand  
 
 # intializes the Hand class with shuffled cards of the deck
 # cards.sort_by! sorts the cards based upon their poker values
 # cards.dup gives me the cards to test by creating a copy of the cards in the hand
-	def initialize(cards = Deck.new.shuffle)
+	def initialize(cards = Deck.all_cards.shuffle)
 		@cards = cards
 		@cards.sort_by! {|card| Card::POKER_VALUES_STRING[card.value]}	
 		@cards_to_test = @cards.dup
@@ -15,9 +16,11 @@ class Hand
 
 	attr_accessor :cards, :cards_to_test
 
-# used to deal 5 cards into the hand
-	def self.deal(deck = Deck.new.shuffle)
-		poker_hand = []
-   		poker_hand << Hand.new(deck.take(5))
+
+# used to deal 5 cards into the hand; deck = Deck.all_cards.shuffle
+	def self.deal(deck)
+		Hand.new(deck.take(5))
+   		# p "poker_hand is #{poker_hand} with #{poker_hand.class}"
   	end
 end
+
--- tddpoker_hand_class.rb
+++ tddpoker_hand_class.rb
@@ -8,24 +8,29 @@ class TddPokerHandClass < Minitest::Test
 			assert_equal(Hand, x.class)
 		end
 
-		def test_hand_is_getting_dealt_card
-			x = Hand.deal
-			assert_equal(1, x.length)
+
+		def test_hand_is_getting_dealt_cards
+			deck = Deck.all_cards.shuffle
+			x = Hand.deal(deck)
+			assert_equal(Hand, x.class)
+			assert_equal(x.cards, x.cards_to_test)
 		end
+
 
 		def test_cards_dup_creates_duplicates
 			cards = [Card.new(:two, :clubs), Card.new(:three, :clubs), Card.new(:four, :clubs), Card.new(:five, :clubs), Card.new(:six, :clubs)]
 			assert_equal(cards, cards.dup)
 		end
 
-		
-		def test_cards_in_hand
-			cards = [Card.new(:two, :clubs), Card.new(:three, :clubs), Card.new(:four, :clubs), Card.new(:five, :clubs), Card.new(:six, :clubs)]
-     		deck = Deck.new(cards.dup)
-      		hand = Hand.deal(deck)
-        	assert_equal(1, hand.length)
-        	assert_equal(5, cards.length)
-        	
-        	p hand
-        end
+
+		# def test_cards_in_hand
+		# 	cards = [Card.new(:eight, :clubs), Card.new(:three, :clubs), Card.new(:four, :clubs), Card.new(:five, :clubs), Card.new(:six, :clubs)]
+  #    		deck = Deck.new(cards.dup)
+  #     		hand = Hand.deal(deck)
+  #     		assert_equal(5, cards.length)
+  #     		assert_equal(1, hand.length)
+  #     		# assert_equal(hand.cards, cards)
+  #       end
+
+
 end
