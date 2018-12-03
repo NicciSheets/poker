d9286ae157f91ab1363005916a34c9df57a77c63
Nicci Sheets <niccisheets@aol.com>
2018-12-03 10:33:23 -0500

    testing for the game play is okay

--- poker_game.rb
+++ poker_game.rb
@@ -5,29 +5,26 @@ require_relative "poker_hand_class_2.rb"
 	
  	
 
-   	def game_output
-   		# deck = Deck.new.shuffle
-		@black = Hand.deal()
-		@white = Hand.deal()
-		p "@black is #{@black.hand_rating} and #{@black.score} and #{@black.cards_sorted}"
-		p "@white is #{@white.hand_rating} and #{@white.score} and #{@white.cards_sorted}"
-	end   	
+ #   	def game_output
+	# 	@black = Hand.deal()
+	# 	@white = Hand.deal()
+	# 	p "@black is #{@black.hand_rating} and #{@black.score} and #{@black.cards_sorted}"
+	# 	p "@white is #{@white.hand_rating} and #{@white.score} and #{@white.cards_sorted}"
+	# end   	
+
+	def game_output
+		@black 
+		@white 
+	end
 
 	def game_play(game_output)
 		if @black.hand_rating == @white.hand_rating
-			if @black.poker_hand_tie2[0] == @white.poker_hand_tie2[0]
-				if @black.poker_hand_tie2[1] == @white.poker_hand_tie2[1]
-					return @black.poker_hand_tie2[2] <=> @white.poker_hand_tie2[2]
-				else
-					return @black.poker_hand_tie2[1] <=> @white.poker_hand_tie2[1]
-				end
-			else
-				return @black.poker_hand_tie2[0] <=> @white.poker_hand_tie2[0]
-			end
+			return @black.poker_hand_tie2 <=> @white.poker_hand_tie2
 		else
 			@black.score_rating <=> @white.score_rating
 		end
 	end
+	
 
 	def winner
 		compared = game_play(game_output)
@@ -45,4 +42,23 @@ require_relative "poker_hand_class_2.rb"
 # 1 "Black Wins"......(-1) "White Wins"......0 Tie
 	# p game_output
 	# p game_play(game_output)
-	p winner
+# @black = Hand.new([Card.new(:queen, :clubs), Card.new(:ten, :diamonds), Card.new(:five, :clubs), Card.new(:four, :diamonds), Card.new(:three, :clubs)])
+# @white = Hand.new([Card.new(:queen, :diamonds), Card.new(:jack, :diamonds), Card.new(:six, :clubs), Card.new(:four, :spades), Card.new(:two, :clubs)])
+# 		p winner
+
+# before refactoring the comparison for game_play
+# def game_play(game_output)
+	# 	if @black.hand_rating == @white.hand_rating
+	# 		if @black.poker_hand_tie2[0] == @white.poker_hand_tie2[0]
+	# 			if @black.poker_hand_tie2[1] == @white.poker_hand_tie2[1]
+	# 				return @black.poker_hand_tie2[2] <=> @white.poker_hand_tie2[2]
+	# 			else
+	# 				return @black.poker_hand_tie2[1] <=> @white.poker_hand_tie2[1]
+	# 			end
+	# 		else
+	# 			return @black.poker_hand_tie2[0] <=> @white.poker_hand_tie2[0]
+	# 		end
+	# 	else
+	# 		@black.score_rating <=> @white.score_rating
+	# 	end
+	# end
